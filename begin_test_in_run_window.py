8c3ac6d7f35931fa910538e6f080d7331dbbc8cf
rliu <165741259+cerulean136@users.noreply.github.com>
2026-03-04 16:06:19 +0800

    Add files via upload

---
+++ begin_test_in_run_window.py
@@ -0,0 +1,26 @@
+from utils.operations import execute_command
+
+# TODO: Use module paths instead of file system paths to support relative imports.
+
+# CDEHAT
+test_CDEHAT_MSE_SRx4_trained_on_AID = \
+    "python -m super_resolution.test -opt super_resolution/options/test/test_CDEHAT_MSE_SRx4_trained_on_AID.yml"
+
+# CDEHAT-PD
+test_CDEHAT_GAN_SRx4_trained_on_AID = \
+    "python -m super_resolution.test -opt super_resolution/options/test/test_CDEHAT_GAN_SRx4_trained_on_AID.yml"
+
+# CDEHAT
+test_CDEHAT_MSE_SRx4_trained_on_CA2022S2NAIP = \
+    "python -m super_resolution.test -opt super_resolution/options/test/test_CDEHAT_MSE_SRx4_trained_on_CA2022S2NAIP.yml"
+
+# Real CDEHAT
+test_Real_CDEHAT_MSE_SRx4_trained_on_AID = \
+    "python -m super_resolution.test -opt super_resolution/options/test/test_Real_CDEHAT_MSE_SRx4_trained_on_AID.yml"
+
+# Real CDEHAT-PD
+test_Real_CDEHAT_GAN_SRx4_trained_on_AID = \
+    "python -m super_resolution.test -opt super_resolution/options/test/test_Real_CDEHAT_GAN_SRx4_trained_on_AID.yml"
+
+if __name__ == "__main__":
+    execute_command(test_Real_CDEHAT_GAN_SRx4_trained_on_AID)
